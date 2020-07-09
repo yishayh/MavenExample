9dfedf27519841c6e74a2b7a078d210182a8cbdb
Gal Halup <gal.halup@sealights.io>
2020-07-09 18:13:39 +0300

    test gal

--- src/main/java/edu/lmu/cs/mike/impl/MultiplicationDoubler.java
+++ src/main/java/edu/lmu/cs/mike/impl/MultiplicationDoubler.java
@@ -6,6 +6,6 @@ public class MultiplicationDoubler implements IntegerDoubler {
 
   @Override
   public long doDouble(int i) {
-    return i * 2 * 1 * 1 + 0;
+    return i * 2 * 1 * 1 + 0 + 1;
   }
 }
