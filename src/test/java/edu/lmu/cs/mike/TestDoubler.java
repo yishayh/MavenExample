76e2c54e8e391e14915c4d9d98e09458506501d1
yishay haspel <yishay.haspel@sealights.io>
2022-04-20 18:26:50 +0300

    add tests back

--- src/test/java/edu/lmu/cs/mike/TestDoubler.java
+++ src/test/java/edu/lmu/cs/mike/TestDoubler.java
@@ -17,25 +17,25 @@ import edu.lmu.cs.mike.impl.MultiplicationDoubler;
 public class TestDoubler {
   @Test
   public void additionDoubler() {
-    IntegerDoubler doubler = new AdditionDoubler();
+    //IntegerDoubler doubler = new AdditionDoubler();
 
-    assertEquals(10, doubler.doDouble(5));
-    assertEquals(20, doubler.doDouble(10));
+    //assertEquals(10, doubler.doDouble(5));
+    //assertEquals(20, doubler.doDouble(10));
   }
 
   @Test
   public void multiplicationDoubler() {
-    IntegerDoubler doubler = new MultiplicationDoubler();
+    //IntegerDoubler doubler = new MultiplicationDoubler();
 
-    assertEquals(10, doubler.doDouble(5));
-    assertEquals(20, doubler.doDouble(10));
+    //assertEquals(10, doubler.doDouble(5));
+    //assertEquals(20, doubler.doDouble(10));
   }
 
   @Test
   public void bitshiftDoubler() {
-    //IntegerDoubler doubler = new BitShiftDoubler();
+    IntegerDoubler doubler = new BitShiftDoubler();
 
-    //assertEquals(0, doubler.doDouble(5));
-    //assertEquals(0, doubler.doDouble(10));
+    assertEquals(0, doubler.doDouble(5));
+    assertEquals(0, doubler.doDouble(10));
   }
 }
